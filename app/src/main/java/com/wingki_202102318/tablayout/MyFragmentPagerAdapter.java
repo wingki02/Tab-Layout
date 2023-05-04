1e1d7225bc3a255fb59c1e858b3ce11aa8c8fc1f
Wingki <wingkisandros@gmail.com>
2023-05-04 10:22:44 +0700

    Initial commit

---
+++ app/src/main/java/com/wingki_202102318/tablayout/MyFragmentPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.wingki_202102318.tablayout;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter
+{
+    private BeritaFragment _beritaFragment = new BeritaFragment();
+    private Context _context;
+    private EcommerceFragment _eCommerceFragment = new EcommerceFragment();
+    private  int _tabCount;
+    private  KampusFragment _kampusFragment = new KampusFragment();
+
+    public  MyFragmentPagerAdapter(Context context, FragmentManager fragmentManager, int tabCount)
+    {
+        super(fragmentManager);
+
+        _context = context;
+        _tabCount = tabCount;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position)
+    {
+        switch (position)
+        {
+            case 0:
+                return _eCommerceFragment;
+            case 1:
+                return  _beritaFragment;
+            default:
+                return _kampusFragment;
+        }
+    }
+
+    @Override
+    public int getCount()
+    {
+        return _tabCount;
+    }
+}
